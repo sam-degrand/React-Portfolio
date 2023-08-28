45101429e80762f5f6a70af90c27fd4f5dd71475
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-08-27 22:27:34 -0400

    Added projects and images to public file

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const projects = [
+    {
+      title: "Horiseon",
+      subtitle: "Website",
+      description:
+        "A basic web app",
+      image: "./Horiseon.png",
+      link: "https://sam-degrand.github.io/Homework-01/",
+    },
+    {
+      title: "Quote&Speech",
+      subtitle: "Quote Generator",
+      description:
+        "Listen, Learn, and Be Inspired: Text-to-Speech with Daily Quotes",
+      image: "./Quote&Speech.png",
+      link: "https://kzb11128.github.io/quote-and-speech/",
+    },
+    {
+      title: "QuoteHub",
+      subtitle: "Handlebars, Node.js, MySQL, Tailwind CSS, and Alpine.js",
+      description:
+        "This web application allows users to generate random quotes and save their favorites for future reference.",
+      image: "./QuoteHub",
+      link: "https://pure-scrubland-99203-50ab01d03184.herokuapp.com/",
+    },
+
+  ];
