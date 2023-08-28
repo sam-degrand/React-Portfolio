8428f5d54e6e5965f1db1dfd2953e880bbc071d6
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-08-27 22:46:37 -0400

    Pulled public file from practice app

--- src/components/About.js
+++ src/components/About.js
@@ -34,7 +34,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="./Sam Photo.png"
           />
         </div>
       </div>
